cfd7e15f1c71062b37686dccf60d5c78313fc6d4
Sushil Kamble <iamsushil303@gmail.com>
2024-12-16 01:02:26 +0530

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
+
+const prompt = "Explain how AI works";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
